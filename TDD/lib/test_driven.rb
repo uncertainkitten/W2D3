3956dcb4033dca899c705da56a42e7a6effb69a1
Cassandra McClure <uncertainkitten@gmail.com>
2018-08-29 14:52:49 -0700

    Add my transponse and stock picker

--- TDD/lib/test_driven.rb
+++ TDD/lib/test_driven.rb
@@ -15,3 +15,31 @@ def two_sum(arr)
   
   result
 end
+
+def my_transpose(matrix)
+  result = Array.new(matrix.length) {[]} 
+  matrix.each_with_index do |row,row_idx|
+    row.each_with_index do |el, col_idx|
+      result[col_idx][row_idx] = el
+    end
+  end
+  result
+end
+
+def stock_picker(arr)
+  max = 0
+  sell = 0
+  buy = 0
+  
+  arr.each_with_index do |el1, idx1|
+    arr.each_with_index do |el2, idx2|
+      if el2 - el1 > max && idx2 >= idx1
+        buy = idx1
+        sell = idx2  
+        max = el2 - el1 
+      end
+    end
+  end
+  [buy, sell]
+    
+end
--- TDD/spec/test_driven_spec.rb
+++ TDD/spec/test_driven_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "test driven" do
   describe '#remove_dups' do
     it 'return an array with all uniq values' do
       expect(remove_dups([1, 2, 2, 5, 6, 9, 5])).to eq([1, 2, 5, 6, 9])
-  end
+    end
     it "doesn't call Array#uniq" do 
       arr = [1, 2, 2, 5, 6, 9, 5]
       expect(arr).not_to receive(:uniq)
@@ -16,6 +16,30 @@ RSpec.describe "test driven" do
     let(:arr) {[0,1,-1,2]}
     it 'return an array with all uniq values' do
       expect(two_sum(arr)).to eq([[1,2]])
+    end
+  end
+  
+  describe '#my_transpose' do
+    let (:matrix) {[[1,2,3],[4,5,6],[7,8,9]]}
+    it "transposes an array correctly" do
+      expect(my_transpose(matrix)).to eq([[1,4,7],[2,5,8],[3,6,9]])
+    end
+    
+    it "doesn't call Array#transpose" do
+      expect(matrix).not_to receive(:transpose)
+      my_transpose(matrix)
+    end
+  end
+  
+  describe '#stock_picker' do
+    let(:prices) {[5,10,2,20,2]}
+    it "gets the highest value pair" do
+      expect(stock_picker(prices)).to eq([2,3])
+    end
+    
+    it "buys before it sells" do
+      temp = stock_picker(prices)
+      expect((temp[0] < temp[1])).to be(true)
     end
   end
 end
